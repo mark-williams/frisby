78d36f5456f6701a2149dc34080a6c80d7069208
mark-williams <markwill@hotmail.co.uk>
2018-03-31 16:29:19 +0100

    Use boxing to compose functions using 'map'

--- src/01-box/index.js
+++ src/01-box/index.js
@@ -1,9 +1,16 @@
 const incrementAndReturnCharacter = stringcode => {
-  const keycode = stringcode.trim();
-  const code = parseInt(keycode, 10);
-  const incrementedCode = code + 1;
+  // const keycode = stringcode.trim();
+  // const code = parseInt(keycode, 10);
+  // const incrementedCode = code + 1;
 
-  return String.fromCharCode(incrementedCode);
+  // return String.fromCharCode(incrementedCode);
+  const result = [stringcode]
+    .map(s => s.trim())
+    .map(s => parseInt(s, 10))
+    .map(i => i + 1)
+    .map(k => String.fromCharCode(k));
+
+  return result[0];
 };
 
 export default incrementAndReturnCharacter;
