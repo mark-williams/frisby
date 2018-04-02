bbd13d8f4c3672f1ef6224de101d4081374b44e5
mark-williams <markwill@hotmail.co.uk>
2018-04-02 10:02:40 +0100

    04 - extracted try/catch into a function

--- src/04-chain-composable-error-handling/index.js
+++ src/04-chain-composable-error-handling/index.js
@@ -1,13 +1,30 @@
 import fs from 'fs';
 
-const getVersion = fileName => {
+const right = x => ({
+  map: f => right(f(x)),
+  fold: (f, g) => g(x)
+});
+
+const left = x => ({
+  map: () => left(x),
+  fold: (f) => f(x)
+});
+
+const tryCatch = f => {
   try {
-    const str = fs.readFileSync(fileName);
-    const pkg = JSON.parse(str);
-    return pkg.version;
-  } catch (err) {
-    return 'ERROR';
+    return right(f());
+  } catch (e) {
+    return left(e);
   }
+};
+
+const getVersion = fileName => {
+  return tryCatch(() => fs.readFileSync(fileName))
+    .map(c => JSON.parse(c))
+    .fold(
+      () => 'ERROR',
+      p => p.version
+    );
 };
 
 export default getVersion;
