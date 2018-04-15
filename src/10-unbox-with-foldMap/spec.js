5cf711c23dfd5dc268520532a97e5acf7372fdd6
mark-williams <markwill@hotmail.co.uk>
2018-04-15 10:26:23 +0100

    10 - use fold

--- src/10-unbox-with-foldMap/spec.js
+++ src/10-unbox-with-foldMap/spec.js
@@ -1,8 +1,14 @@
+import { List } from 'immutable-ext';
 import { Sum } from './';
 
 describe('10 - unbox with foldMap', () => {
   it('should get total using reduce', () => {
     const result = [Sum(4), Sum(7), Sum(11)].reduce((acc, x) => acc.concat(x), Sum.empty());
+    expect(result.val).toBe(22);
+  });
+
+  it('should get total using fold', () => {
+    const result = List.of(Sum(4), Sum(7), Sum(11)).fold(Sum.empty());
     expect(result.val).toBe(22);
   });
 });
