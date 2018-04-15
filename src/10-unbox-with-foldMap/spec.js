9b8efaca1ff8c16a91f29e41db216c2487c3e928
mark-williams <markwill@hotmail.co.uk>
2018-04-15 10:35:03 +0100

    10 - use foldMap

--- src/10-unbox-with-foldMap/spec.js
+++ src/10-unbox-with-foldMap/spec.js
@@ -11,4 +11,17 @@ describe('10 - unbox with foldMap', () => {
     const result = List.of(Sum(4), Sum(7), Sum(11)).fold(Sum.empty());
     expect(result.val).toBe(22);
   });
+
+  it('should take non-boxed list and use map then fold', () => {
+    const result = List([4, 5, 6])
+      .map(Sum)
+      .fold();
+    expect(result.val).toBe(15);
+  });
+
+  it('should use foldMap that combines the map then fold', () => {
+    const result = List([4, 5, 6])
+      .foldMap(Sum);
+    expect(result.val).toBe(15);
+  });
 });
