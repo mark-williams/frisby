b2dcbf0f623c581f60b644be71050f8be859acce
mark-williams <markwill@hotmail.co.uk>
2018-04-21 14:57:19 +0100

    folktale: implement function returning Maybe

--- src/folktale/maybe/index.js
+++ src/folktale/maybe/index.js
@@ -1,3 +1,5 @@
+import Maybe from 'folktale/maybe';
+
 const findInArray = (arr, predicate) => {
   for (let i = 0; i < arr.length; i++) {
     if (predicate(arr[i])) {
@@ -7,4 +9,15 @@ const findInArray = (arr, predicate) => {
   return null;
 };
 
+const maybeFindInArray = (arr, predicate) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (predicate(arr[i])) {
+      //return new Date();
+      return Maybe.Just(arr[i]);
+    }
+  }
+  return Maybe.Nothing();
+};
+
+export { maybeFindInArray };
 export default findInArray;
--- src/folktale/maybe/spec.js
+++ src/folktale/maybe/spec.js
@@ -1,11 +1,28 @@
-import findInArray from './';
+import findInArray, { maybeFindInArray } from './';
+import Maybe from 'folktale/maybe';
+
+const isEqualTo = j => k => j === k;
 
 describe('folktale | maybe', () => {
   describe('maybe', () => {
     it('findInArray returns item matching predicate', () => {
-      const isEqualTo = j => k => j === k;
       const result = findInArray([1, 2, 3, 4], isEqualTo(3));
       expect(result).toBe(3);
+    });
+
+    it('maybeFindInArray returns Maybe', () => {
+      const result = maybeFindInArray([1, 2, 3, 4], isEqualTo(13));
+      expect(Maybe.hasInstance(result));
+    });
+
+    it('maybeFindInArray returns Maybe.Just', () => {
+      const result = maybeFindInArray([1, 2, 3, 4], isEqualTo(3));
+      expect(result.getOrElse('Not found')).toBe(3);
+    });
+
+    it('if item not found maybeFindInArray returns Maybe.Nothing', () => {
+      const result = maybeFindInArray([1, 2, 3, 4], isEqualTo(6));
+      expect(result.getOrElse('Not found')).toBe('Not found');
     });
   });
 });
