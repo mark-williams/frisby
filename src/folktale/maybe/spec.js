006b9c5edd5646155028efeeaacad0138727373a
mark-williams <markwill@hotmail.co.uk>
2018-04-22 09:21:33 +0100

    folktale: sequencing operations (map, chain)

--- src/folktale/maybe/spec.js
+++ src/folktale/maybe/spec.js
@@ -34,12 +34,38 @@ describe('folktale | maybe', () => {
     });
   });
 
-  describe('transforming values', () => {
-    it('increment found item', () => {
-      const result = maybeFindInArray([7, 3, 5, 2], isEqualTo(5));
-      const incremented = result.map(x => x + 1);
-      expect(Maybe.hasInstance(incremented)).toBe(true);
-      expect(incremented.getOrElse('')).toEqual(6);
+  describe('transforming, chaining', () => {
+    const increment = x => Maybe.Just(x + 1);
+    const transform = y => Maybe.Just({ value: y });
+
+    describe('transforming values', () => {
+      it('increment found item', () => {
+        const result = maybeFindInArray([7, 3, 5, 2], isEqualTo(5));
+        // NB transforming function DOESN'T return Maybe
+        const incremented = result.map(x => x + 1);
+
+        expect(Maybe.hasInstance(incremented)).toBe(true);
+        expect(incremented.getOrElse('')).toEqual(6);
+      });
+    });
+
+    describe("sequencing transforms (each function applied returns a Maybe, so use 'chain')", () => {
+      it('increment found number and transform into object', () => {
+        const result = maybeFindInArray([7, 3, 5, 2], isEqualTo(2));
+        const transformed = result
+          .chain(increment)
+          .chain(increment)
+          .chain(transform);
+
+        expect(transformed.getOrElse('')).toEqual({ value: 4 });
+      });
+
+      it('will only apply transforms to Maybe.Just', () => {
+        const result = maybeFindInArray([], isEqualTo(2));
+        const transformed = result.chain(increment).chain(transform);
+
+        expect(transformed.getOrElse('Not found')).toEqual('Not found');
+      });
     });
   });
 });
