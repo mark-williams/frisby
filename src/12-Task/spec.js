99b3c3f97bb9226efe386e216c8a4ebe53accf5c
mark-williams <markwill@hotmail.co.uk>
2018-04-29 10:25:47 +0100

    Simple non-task based imp

--- src/12-Task/spec.js
+++ src/12-Task/spec.js
@@ -1,15 +1,39 @@
+//import { task } from 'folktale/concurrency/task';
+
+const TOPCORNER = 'topcorner';
+const MISSING = 'missing';
 const PENALTY_SCORED = 'Scores!!!';
+// const PENALTY_SAVED = 'Saved!!!';
 
 /* eslint no-console: off */
-const takePenalty = (rej, res) => {
-  console.log('Shoots...');
-  res(PENALTY_SCORED);
+const penalty = attempt => (rej, res) => {
+  if (attempt.shot === MISSING) {
+    rej('Missed');
+  } else {
+    res(PENALTY_SCORED);
+  }
 };
 
 describe('Task (from folktale', () => {
-  it('calls success', () => {
-    const success = jest.fn();
-    takePenalty(() => {}, success);
-    expect(success).toBeCalledWith(PENALTY_SCORED);
+  let success;
+  let reject;
+
+  beforeEach(() => {
+    success = jest.fn();
+    reject = jest.fn();
+  });
+
+  it("take 'good' penalty", () => {
+    const takePen = penalty({ shot: TOPCORNER });
+    takePen(reject, success);
+    expect(reject).toHaveBeenCalledTimes(0);
+    expect(success).toHaveBeenCalledTimes(1);
+  });
+
+  it("take 'poor' penalty", () => {
+    const takePen = penalty({ shot: MISSING });
+    takePen(reject, success);
+    expect(reject).toHaveBeenCalledTimes(1);
+    expect(success).toHaveBeenCalledTimes(0);
   });
 });
